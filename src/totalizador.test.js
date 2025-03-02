385a965ba2a30152874d781551f118bc4c183fbe
Denisse91011 <denissecardenasmayan947@gmail.com>
2025-03-01 22:30:54 -0400

    FEAT Y TEST: Funcionalidad de saludar al cliente y realizar la primera prueba

---
+++ src/totalizador.test.js
@@ -0,0 +1,5 @@
+import { saludarCliente } from "./totalizador.js";
+
+test("Debe saludar al cliente con un mensaje de bienvenida", () => {
+    expect(saludarCliente("Valeria")).toBe("¡Bienvenido, Valeria!");
+});
