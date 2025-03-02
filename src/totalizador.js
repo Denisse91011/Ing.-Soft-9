afcf977324f86ab22c5f4d759744da1c3f555635
Denisse91011 <denissecardenasmayan947@gmail.com>
2025-03-01 22:14:25 -0400

    REFACTOR: Saludador mejorado para que ingrese su nombre y se muestre un mensaje

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,3 +1,12 @@
+import { saludarCliente } from "./totalizador.js";
+
+document.getElementById("btnSaludar").addEventListener("click", () => {
+    const nombre = document.getElementById("nombre").value;
+    const mensaje = saludarCliente(nombre);
+    document.getElementById("mensajeSaludo").textContent = mensaje;
+});
+
 export function saludarCliente(nombre) {
     return `¡Bienvenida, ${nombre}!`;
 }
+
